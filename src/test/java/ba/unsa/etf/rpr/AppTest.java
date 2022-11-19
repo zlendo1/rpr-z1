f1bc62cd5ac396e51003539fb80f8935405485ea
zlendo1 <zlendo1@etf.unsa.ba>
2022-11-19 23:53:07 +0100

    3.5 small changes to test classes and regenerated the documentation

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -2,13 +2,13 @@ package ba.unsa.etf.rpr;
 
 import org.junit.jupiter.api.Test;
 
-public class AppTest {
+class AppTest {
 
     /**
      * Test za main, regularno
      */
     @Test
-    public void main1() {
+    void main1() {
         String[] argumenti = new String[] {"(", "2", "+", "3", ")"};
         App.main(argumenti);
     }
@@ -17,7 +17,7 @@ public class AppTest {
      * Test za main, sa greskom
      */
     @Test
-    public void main2() {
+    void main2() {
         String[] argumenti = new String[] {"((", "2", "+", "3", ")"};
         App.main(argumenti);
     }
